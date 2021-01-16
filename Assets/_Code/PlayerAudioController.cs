b8d4cf6d37df4eafa0a7fdd5db10d8e5d6cd0ae1
Janek <jan.gajewski3@gmail.com>
2021-01-17 00:42:35 +0100

    Project copied from PPzD with FMOD plugin

---
+++ Assets/_Code/PlayerAudioController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerAudioController : PlayerSubsystem {
+
+    public override void HandleEvent(PlayerEventType eventType) {
+        switch (eventType) {
+            case PlayerEventType.Jump:
+                //TODO play jump sound
+                break;
+            case PlayerEventType.Landing:
+                //TODO play landing sound
+                break;
+            case PlayerEventType.Death:
+                break;
+            case PlayerEventType.Attack:
+                break;
+            case PlayerEventType.Footstep:
+                //TODO play random footstep sound
+                break;
+        }
+    }
+}
